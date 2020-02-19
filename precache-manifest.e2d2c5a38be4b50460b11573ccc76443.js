69c08171afa36c8a2625ab1ba5fdebe4a2219593
Luiz C W S <luizcarloswagner@gmail.com>
2020-02-19 04:22:25 -0300

    Updates

---
+++ precache-manifest.e2d2c5a38be4b50460b11573ccc76443.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3da3bd2e1f46a7c6e41d10dd3e691d55",
+    "url": "/challenge-zup/index.html"
+  },
+  {
+    "revision": "b3fb7843eedb46442517",
+    "url": "/challenge-zup/static/js/2.c5289a6c.chunk.js"
+  },
+  {
+    "revision": "48a7f982264b141da44db572a26ecb94",
+    "url": "/challenge-zup/static/js/2.c5289a6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23a28241229d6784a657",
+    "url": "/challenge-zup/static/js/main.3b9c3207.chunk.js"
+  },
+  {
+    "revision": "ebc7aa49074d301e6970",
+    "url": "/challenge-zup/static/js/runtime-main.03639d23.js"
+  }
+]);
